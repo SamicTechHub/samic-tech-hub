f0eceac27ffe6ab5362d50285600928f37c7661e
DrFemzy <adesokanemmanuelfemzy2018@gmail.com>
2022-10-19 22:57:16 +0100

    Home page Desktop site done - DrFemzy

---
+++ components/Footer.js
@@ -0,0 +1,50 @@
+import React from 'react'
+import style from "../styles/component.module.css"
+import {
+    MailFilled, PhoneFilled, GooglePlusSquareFilled, TwitterCircleFilled, FacebookFilled, InstagramFilled, GithubFilled
+} from "@ant-design/icons"
+
+function Footer() {
+  return (
+    <div className={style.footerContainer}>
+      <div className={style.newsletter}>
+        <input placeholder='Enter Email Here to Receive Information and Updates about our Programmes' />
+        <button className='primaryBtn'>Subscribe Now</button>
+      </div>
+
+      <div className={style.aboutNav}>
+        <div className={style.aboutColumn}>
+            <h3>Samic</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Blandit nunc id purus nulla.</p>
+            <div className={style.socials}>
+            <TwitterCircleFilled /> <FacebookFilled /> <InstagramFilled /> <GithubFilled /> 
+
+            </div>
+        </div>
+        <div className={style.aboutColumn}>
+            <h3>Company</h3>
+            <p>Who we are</p>
+            <p>Careers</p>
+            <p>Blog</p>
+        </div>
+        <div className={style.aboutColumn}>
+            <h3>Programs</h3>
+            <p>Web Designing</p>
+            <p>UI/UX Design</p>
+            <p>Digital Marketing</p>
+        </div>
+        <div className={style.aboutColumn}>
+            <h3>Contact</h3>
+            <p><MailFilled /> Info@samictechhub.com</p>
+            <p><PhoneFilled /> +234 801 234 5678</p>
+            <p><GooglePlusSquareFilled /> Info@samictechhub.com</p>
+        </div>
+      </div>
+        <div className={style.copyright}>
+            <p>© Copyright 2022, All Rights Reserved</p>
+        </div>
+    </div>
+  )
+}
+
+export default Footer
--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import Link from 'next/link'
 import { useEffect, useState } from 'react'
+import Footer from '../components/Footer'
 import ProfileTestimonial from '../components/ProfileTestimonial'
 import style from '../styles/Home.module.css'
 
@@ -107,10 +108,182 @@ export default function Home() {
           <div className={style.imageContainer}>
             <Image src={"/img/aboutImage.png"} alt={"Samic Tech Hub"} layout={"fill"} objectFit={"contain"} objectPosition={"bottom"} />
           <div className={style.aboutStat}>
-            <div></div>
+            <div className={style.statContainer}>
+              <h1>200+</h1>
+              <p>Students Trained</p>
+            </div>
+            <div className={style.statContainer}>
+              <h1>30+</h1>
+              <p>Job offer Received</p>
+            </div>
           </div>
           </div>
         </div>
+      </section>
+
+      {/* Our Programs */}
+      <section className={style.ourProgramSection}>
+        <Image src={"/img/Union1.png"} alt={"Samic Tech Hub"} layout={"fill"} objectFit={"contain"} />
+        <Image src={"/img/Union2.png"} alt={"Samic Tech Hub"} layout={"fill"} objectFit={"contain"} />
+        <div className={style.description}>
+          <h2>Our Programs</h2>
+          <p>We reduce risk through consulting & security product expertise. We are a trusted advisor for all cyber security</p>
+        </div>
+
+        <div className={style.programs}>
+
+          <div className={style.program}>
+            <div className={style.svgContainer}>
+              <Image src={"/img/Vector.svg"} alt={"Samic Tech Hub"} layout={"fill"} objectFit={"contain"}/>
+            </div>
+            <h3>Web Designing</h3>
+            <p>This program is designed to provide you with in-depth knowledge on how to create functional websites by implementing web designs through the use of various programming languages.</p>
+
+            <button style={{color: "white"}}>Read More →</button>
+          </div>
+          <div className={style.program}>
+            <div className={style.svgContainer}>
+              <Image src={"/img/Vector.svg"} alt={"Samic Tech Hub"} layout={"fill"} objectFit={"contain"}/>
+            </div>
+            <h3>Digital Marketing</h3>
+            <p>This program is designed to provide you with in-depth knowledge on how to create functional websites by implementing web designs through the use of various programming languages.</p>
+
+            <button style={{color: "white"}}>Read More →</button>
+          </div>
+          <div className={style.program}>
+            <div className={style.svgContainer}>
+              <Image src={"/img/Vector2.svg"} alt={"Samic Tech Hub"} layout={"fill"} objectFit={"contain"}/>
+            </div>
+            <h3>UX/UI Design</h3>
+            <p>This program is designed to provide you with in-depth knowledge on how to create functional websites by implementing web designs through the use of various programming languages.</p>
+
+            <button style={{color: "#27BEAB"}}>Read More →</button>
+          </div>
+
+
+        </div>
+
+      </section>
+
+      {/* Take the Lead Section */}
+      <section className={style.takeLeadSection}>
+        <div className={style.takeLeadIntro}>
+          <h3>Take the lead with Us</h3>
+          <p>With the best-in-class training, and our commitment to lifelong learning, we have created an unrivaled opportunity to prepare, launch and grow your career.</p>
+        </div>
+        <div className={style.takeLeadItems}>
+          <div className={style.takeLeadItem}>
+            <div className={style.takeLeadItemImage}>
+              <Image src={"/img/trend.png"} alt={"Samic Tech Hub"} layout={"fill"}  />
+            </div>
+            <div className={style.takeLeadItemTexts}>
+              <h3>Future Proof</h3>
+              <p>Focused on the high paying, in-demand fields of today and tomorrow.</p>
+            </div>
+          </div>
+          <div className={style.takeLeadItem}>
+            <div className={style.takeLeadItemImage}>
+              <Image src={"/img/development.png"} alt={"Samic Tech Hub"} layout={"fill"}  />
+            </div>
+            <div className={style.takeLeadItemTexts}>
+              <h3>Career Development</h3>
+              <p>Gain access to a network of opportunities, and a community of peers and mentors to support your career growth.</p>
+            </div>
+          </div>
+          <div className={style.takeLeadItem}>
+            <div className={style.takeLeadItemImage}>
+              <Image src={"/img/work.png"} alt={"Samic Tech Hub"} layout={"fill"}  />
+            </div>
+            <div className={style.takeLeadItemTexts}>
+              <h3>Ready to Work</h3>
+              <p>Focused on the high paying, in-demand fields of today and tomorrow.</p>
+            </div>
+          </div>
+          <div className={style.takeLeadItem}>
+            <div className={style.takeLeadItemImage}>
+              <Image src={"/img/flexible.png"} alt={"Samic Tech Hub"} layout={"fill"}  />
+            </div>
+            <div className={style.takeLeadItemTexts}>
+              <h3>Flexible</h3>
+              <p>We understand you have a million other things to do. We have made learning flexible for you.</p>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      {/* Testimonial Section */}
+      <section className={style.testimonialSection}>
+        <div className={style.testimonialIntro}>
+          <h3>Don&apos;t just take our word for it, here&apos;s what our students say about us.</h3>
+        </div>
+        <div className={style.testimonials}>
+          <div className={style.testimonial} >
+            <div className={style.testimonialImageContainer}>
+              <Image 
+                src={"/img/testimonial1.png"}
+                alt={"Samic Tech Hub"}
+                layout={"fill"}
+                objectFit={"cover"}
+              />
+            </div>
+
+            <h3>&quot;The Best in town&quot;</h3>
+
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Faucibus sed sit ultrices et sed metus sollicitudin. Orci nullam vitae amet ullamcorper scelerisque </p>
+
+            <div className={style.testimonialUserDetails}>
+              <h5>Emeka Afolayan</h5>
+              <p>Freelance UX Designer</p>
+            </div>
+
+          </div>
+          <div className={style.testimonial} >
+            <div className={style.testimonialImageContainer}>
+              <Image 
+                src={"/img/testimonial2.png"}
+                alt={"Samic Tech Hub"}
+                layout={"fill"}
+                objectFit={"cover"}
+              />
+            </div>
+
+            <h3>&quot;Instant Job Offer&quot;</h3>
+
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Faucibus sed sit ultrices et sed metus sollicitudin. Orci nullam vitae amet ullamcorper scelerisque </p>
+
+            <div className={style.testimonialUserDetails}>
+              <h5>Mariam Achur</h5>
+              <p>Digital Marketer, NotMini</p>
+            </div>
+
+          </div>
+          <div className={style.testimonial} >
+            <div className={style.testimonialImageContainer}>
+              <Image 
+                src={"/img/testimonial3.png"}
+                alt={"Samic Tech Hub"}
+                layout={"fill"}
+                objectFit={"cover"}
+              />
+            </div>
+
+            <h3>&quot;They made it easy&quot;</h3>
+
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Faucibus sed sit ultrices et sed metus sollicitudin. Orci nullam vitae amet ullamcorper scelerisque </p>
+
+            <div className={style.testimonialUserDetails}>
+              <h5>Oluwatoyin Abudu</h5>
+              <p>Front End Designer, ButterStack</p>
+            </div>
+
+          </div>
+        </div>
+      </section>
+
+
+      {/* Footer Section */}
+      <section className={style.footerSection}>
+        <Footer />
       </section>
     </>
   )
