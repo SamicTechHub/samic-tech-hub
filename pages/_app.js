50f703391c5098c140b9e82eac5a8147c2e09ec7
devFemzy <adesokanemmanuelfemzy2018@gmail.com>
2025-01-13 18:53:12 +0100

    pixel

--- pages/_app.js
+++ pages/_app.js
@@ -1,22 +1,51 @@
-import '../styles/globals.css'
-import React, {useEffect} from 'react'
-import { Provider } from 'react-redux'
-import { PersistGate } from "reduxjs-toolkit-persist/integration/react"
-import { store, persistor } from '../redux/store/store'
-import { useRouter } from 'next/router'
-import NextNProgress from "nextjs-progressbar"
+import "../styles/globals.css";
+import React, { useEffect } from "react";
+import { Provider } from "react-redux";
+import { PersistGate } from "reduxjs-toolkit-persist/integration/react";
+import { store, persistor } from "../redux/store/store";
+import { useRouter } from "next/router";
+import NextNProgress from "nextjs-progressbar";
+import Head from "next/head";
+import Script from "next/script";
 
 function MyApp({ Component, pageProps }) {
-  return(
+  return (
     <>
+      <Head>
+        <Script
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              !function(f,b,e,v,n,t,s)
+              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+              n.queue=[];t=b.createElement(e);t.async=!0;
+              t.src=v;s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)}(window, document,'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+              fbq('init', '3395158293952350');
+              fbq('track', 'PageView');
+            `,
+          }}
+        />
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=3395158293952350&ev=PageView&noscript=1"
+          />
+        </noscript>
+      </Head>
       <Provider store={store}>
         <PersistGate persistor={persistor}>
-          <NextNProgress color='green' />
+          <NextNProgress color="green" />
           <Component {...pageProps} />
         </PersistGate>
       </Provider>
     </>
-  )
+  );
 }
 
-export default MyApp
+export default MyApp;
