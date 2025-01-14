b8d3949f8111533eb43ca9fb7a0eb0543d6ef136
devFemzy <adesokanemmanuelfemzy2018@gmail.com>
2025-01-14 09:02:05 +0100

    pixel error fixed

--- pages/_app.js
+++ pages/_app.js
@@ -9,25 +9,21 @@ import Head from "next/head";
 import Script from "next/script";
 
 function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    import('react-facebook-pixel')
+      .then((x) => x.default)
+      .then((ReactPixel) => {
+        ReactPixel.init('3395158293952350');
+        ReactPixel.pageView();
+      });
+  }, []);
+
   return (
     <>
       <Head>
         <Script
           strategy="afterInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `
-              !function(f,b,e,v,n,t,s)
-              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-              n.queue=[];t=b.createElement(e);t.async=!0;
-              t.src=v;s=b.getElementsByTagName(e)[0];
-              s.parentNode.insertBefore(t,s)}(window, document,'script',
-              'https://connect.facebook.net/en_US/fbevents.js');
-              fbq('init', '3395158293952350');
-              fbq('track', 'PageView');
-            `,
-          }}
+          src="https://connect.facebook.net/en_US/fbevents.js"
         />
         <noscript>
           <img
