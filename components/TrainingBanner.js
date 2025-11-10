5ec77a64506965602f2584272ce3ed3c72435af3
unknown <debbieanlaw@gmail.com>
2025-11-10 15:00:14 +0100

    training notification

---
+++ components/TrainingBanner.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import { useEffect, useState } from "react";
+import styles from "../styles/trainingBanner.module.css"
+import RocketLaunchIcon from "@mui/icons-material/RocketLaunch";
+
+const TrainingBanner = () => {
+      const [showBanner, setShowBanner] = useState(false);
+   
+  return (
+    <div
+      className={styles.trainingBanner}
+      onClick={() => window.open("https://forms.gle/Yts4N5nocq36NyKy5", "_blank")}
+
+    >
+      {/* <RocketLaunchIcon style={{ fontSize: 15, color: "red" }} /> */}
+       Free Tech Training Coming Soon!{" "}
+      <span>Click here to register.</span>
+    </div>
+)
+}
+
+export default TrainingBanner
