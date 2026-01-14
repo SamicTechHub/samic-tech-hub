f98c19b46e07457689e51821d1b1945457a4fc08
unknown <debbieanlaw@gmail.com>
2026-01-14 09:44:00 +0100

    build

--- next.config.js
+++ next.config.js
@@ -1,8 +1,33 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//    eslint: {
+//     ignoreDuringBuilds: true,
+//   },
+//   reactStrictMode: true,
+//   swcMinify: false,
+  
+// }
+
+// module.exports = nextConfig
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
   reactStrictMode: true,
-  swcMinify: false,
-  
-}
+  modularizeImports: {
+    '@mui/icons-material': {
+      transform: '@mui/icons-material/{{member}}',
+    },
+  },
+  webpack: (config, { isServer }) => {
+    // Increase file watcher limit
+    config.watchOptions = {
+      ...config.watchOptions,
+      ignored: ['**/node_modules', '**/.git'],
+    };
+    return config;
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
